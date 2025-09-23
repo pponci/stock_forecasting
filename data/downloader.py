48a121b29fc78edde2b0b681689f15c85e311fcb
Piercarlo Ponci <piercarloponci@mac-mini.local>
2025-09-23 16:03:22 +0200

    first version of downloading the stock dat and backing it up in csv in two different drives

--- data/downloader.py
+++ data/downloader.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import yfinance as yf
 import datetime
+import tqdm
 import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from utils.helpers import get_ticker_list
+from utils.helpers import get_ticker_list, get_env_var
 
 
 def download_data(ticker_name : str, start_date : datetime.date, end_date : datetime.date) -> pd.DataFrame:
     """
-    Dowloads the stock data for the given sticker between the specified dates
+    Dowloads the stock data for the given sticker between the specified dates.
     """
 
     ticker = yf.Ticker(ticker = ticker_name)
@@ -23,3 +24,41 @@ def download_data(ticker_name : str, start_date : datetime.date, end_date : date
     return ticker_data
 
 
+def csv_data_backup(data : pd.DataFrame, ticker_name : str, end_date : datetime.date):
+    """
+    Backs up the dowloaded data in csv format in two different drives.
+    """
+
+    backup_dirs = [get_env_var(key = "backup_dir1"), get_env_var(key = "backup_dir2")]
+
+    for backup_dir in backup_dirs:
+
+        month_dir = backup_dir + f"/download_{end_date.strftime("%Y_%m")}"
+
+        if not os.path.isdir(month_dir):
+            os.makedirs(month_dir)
+        
+
+        day_dir = month_dir + f"/download_{end_date.strftime("%Y_%m_%d")}"
+
+        if not os.path.isdir(day_dir):
+            os.makedirs(day_dir)
+        
+
+        file_path = day_dir + f"/{ticker_name}_{end_date.strftime("%Y_%m_%d")}.csv"
+
+        data.to_csv(file_path)
+
+
+if __name__ == "__main__":
+
+    end_date = datetime.datetime.now().date()
+    start_date = end_date + datetime.timedelta(days = -6)
+
+    ticker_list = get_ticker_list()
+
+    for ticker in tqdm.tqdm(ticker_list):
+        data = download_data(ticker_name = ticker, start_date = start_date, end_date = end_date)
+
+        if not data.empty:
+            csv_data_backup(data = data, ticker_name = ticker, end_date = end_date)
--- utils/helpers.py
+++ utils/helpers.py
@@ -5,7 +5,28 @@ def get_ticker_list() -> list:
     Fetches the ticker list
     """
 
-    with open("./storage/tickers/ticker_list.json", "r") as f:
+    with open("./storage/other/ticker_list.json", "r") as f:
         ticker_list = json.load(f)
 
     return ticker_list
+
+
+def get_env_vars() -> dict:
+    """
+    Fetches the enviroment variables
+    """
+
+    with open("env_variables.json", "r") as f:
+        env_vars = json.load(f)
+    
+    return env_vars
+
+
+def get_env_var(key : str):
+    """
+    Fetches one enviroment variable by key.
+    """
+
+    env_vars = get_env_vars()
+
+    return env_vars[key]
